b290a8d24fac50535e7cfb9ce31ecb49b65356ac
CTZDev <carlosteranzavaleta@hotmail.com>
2024-06-21 11:45:39 -0500

    Add btnBack and update btnForward

--- main.js
+++ main.js
@@ -18,10 +18,35 @@ const stepProgressBar = (steps, btnForward, btnBack) => {
         $img.src = $imgSteps[0];
         $img.alt = $altSteps[0];
         $txtStep.classList.add('active');
+        $btnBack.style.backgroundColor = 'slateblue';
+        $btnBack.disabled = false;
+
+        if (indexStep === LIMIT_STEP - 1) {
+          e.target.disabled = true;
+          e.target.style.backgroundColor = 'rgb(184, 184, 184)';
+        }
       }
     }
 
     if (e.target === $btnBack) {
+      $steps[indexStep].classList.add('step-icon-error');
+      $steps[indexStep].classList.remove('step-icon-ok');
+      $steps[indexStep - 1].classList.remove('active');
+      const $img = $steps[indexStep].querySelector('img');
+      const $txtStep = $steps[indexStep].closest('.step').querySelector('.step-text');
+      $img.src = $imgSteps[1];
+      $img.alt = $altSteps[1];
+      $txtStep.classList.remove('active');
+      indexStep--;
+
+      if (indexStep === 0) {
+        e.target.disabled = true;
+        e.target.style.backgroundColor = 'rgb(184, 184, 184)';
+        return;
+      }
+
+      $btnForward.style.backgroundColor = 'slateblue';
+      $btnForward.disabled = false;
     }
   });
 };
